60e6bcae1b07f737734af041ce31187eb0442988
yoshi-island <1655445+yoshi-island@users.noreply.github.com>
2022-10-12 14:06:58 +0900

    formatted

--- abc_test.py
+++ abc_test.py
@@ -5,7 +5,7 @@ from abc import abstractmethod
 #class Car(ABC):
 class Car(metaclass=ABCMeta):
     def __init__(self):
-        print("===Car class begin===")
+        print("Car class begin")
 
     def greeting(self):
         return "Hello!"
@@ -15,7 +15,7 @@ class Car(metaclass=ABCMeta):
         pass
 
     def __del__(self):
-        print("===Car class end===")
+        print("Car class end")
 
 class Firetruck(Car):
     def name(self):
@@ -29,9 +29,6 @@ class PoliceCar(Car):
         return '"woo woo woo"'
 
 class Ambulance(Car):
-    def name(self):
-        return "I'm an ambulance"
-
     def siren(self):
         return '"nee-naw nee-naw"'
 
@@ -40,39 +37,43 @@ class GarbageTruck():
         return "I'm a garbagetruck"
 
 class Bus():
-    def name(self):
-        return "I'm a bus"
-
     def drive(self):
         return '"round and round"'
 
+print("###FIRETRUCK###")
 firetruck = Firetruck()
-print(isinstance(firetruck, Car))
+print(f"is instance: {isinstance(firetruck, Car)}")
+print(f"is subclass: {issubclass(firetruck.__class__, Car)}")
 print(firetruck.greeting())
 print(firetruck.name())
 del firetruck
 
+print("###POLICECAR###")
 policecar = PoliceCar()
-print(isinstance(policecar, Car))
+print(f"is instance: {isinstance(policecar, Car)}")
+print(f"is subclass: {issubclass(policecar.__class__, Car)}")
 print(policecar.greeting())
 print(policecar.name())
 print(policecar.siren())
 del policecar
 
 #ambulance = Ambulance() # Cannot instantiate abstract class "Ambulance" with abstract attribute "name"
-#print(isinstance(ambulance, Car))
-#print(ambulance.greeting())
-#print(ambulance.siren())
-#del ambulance
 
+print("###GARBAGETRUCK###")
 Car.register(GarbageTruck)
 garbagetruck = GarbageTruck()
-print(isinstance(garbagetruck, Car))
+print(f"is instance: {isinstance(garbagetruck, Car)}")
+print(f"is subclass: {issubclass(garbagetruck.__class__, Car)}")
+#print(garbagetruck.greeting()) # "GarbageTruck" has no attribute "greeting"
 print(garbagetruck.name())
 del garbagetruck
 
+print("###BUS###")
 Car.register(Bus) # registerを使うとエラーにならない
 bus = Bus()
-print(isinstance(bus, Car))
+print(f"is instance: {isinstance(bus, Car)}")
+print(f"is subclass: {issubclass(bus.__class__, Car)}")
+#print(bus.greeting()) # "Bus" has no attribute "greeting"
 print(bus.drive())
 del bus
+
